a5429e0d5a712f3a04837d16a91ea9e5e2c0979a
caocong <caocong1@gmail.com>
2019-03-13 14:46:37 +0800

    aaaa

--- src/app.js
+++ src/app.js
@@ -1,22 +1,34 @@
 const map = new AMap.Map('map')
 const $result = $('#result')
 
-$('#refresh').on('click', () => {
+$('#refresh').on('click', e => {
+  e.target.disabled = true
+  $result.text('正在定位')
   navigator.geolocation.getCurrentPosition(pos => {
-    console.log(pos)
     var crd = pos.coords
-    $result.text('Your current position is:Latitude : ' + crd.latitude + 'Longitude: ' + crd.longitude + 'More or less ' + crd.accuracy + ' meters.')
-
-    console.log('Your current position is:')
-    console.log('Latitude : ' + crd.latitude)
-    console.log('Longitude: ' + crd.longitude)
-    console.log('More or less ' + crd.accuracy + ' meters.')
+    // $result.text('Your current position is:Latitude : ' + crd.latitude + 'Longitude: ' + crd.longitude + 'More or less ' + crd.accuracy + ' meters.')
+    getAddressByCoord(crd.longitude + ',' + crd.latitude).then(res => {
+      $result.text(res)
+      e.target.disabled = false
+    }).catch(err => {
+      $result.text('定位失败,ERROR:' + err)
+      e.target.disabled = false
+    })
   }, err => {
-    $result.text('ERROR(' + err.code + '): ' + err.message)
-    console.log(err)
-    console.warn('ERROR(' + err.code + '): ' + err.message)
+    $result.text('定位失败，ERROR(' + err.code + '): ' + err.message)
+    e.target.disabled = false
   }, {
     enableHighAccuracy: true,
     timeout: 5000
+  })
+})
+
+const getAddressByCoord = crd => new Promise((resolve, reject) => {
+  $.get(`https://restapi.amap.com/v3/geocode/regeo?location=${crd}&key=f47030dbde7f633b28c50830183b4934`, res => {
+    if (res.status === '1') {
+      resolve(typeof res.regeocodes.formatted_address === 'string' ? res.regeocodes.formatted_address : res.regeocodes.formatted_address.join(''))
+    } else {
+      reject(res.info)
+    }
   })
 })
